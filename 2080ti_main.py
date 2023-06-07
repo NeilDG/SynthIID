cba93aa9cb6ffcc8d4f830ac5c50771dd754e29a
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-07 23:45:39 +0800

    Preparation for 2080Ti training.

--- 2080ti_main.py
+++ 2080ti_main.py
@@ -3,19 +3,19 @@
 import os
 
 def train_albedo():
-    os.system("python \"iid_train_main.py\" --server_config=5 "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2albedo_v01.06_iid\" --iteration=1")
+    os.system("python \"iid_train_main.py\" --server_config=2 "
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2albedo_v01.05_iid\" --iteration=1")
 
     # FOR TESTING
     # os.system("python \"iid_train_main.py\" --server_config=5 "
     #           "--plot_enabled=1 --save_per_iter=10 --network_version=\"rgb2albedo_v01.02_iid\" --iteration=1")
 
 def test_albedo():
-    os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
+    os.system("python \"iid_test_main.py\" --server_config=2 --img_to_load=1000 "
               "--plot_enabled=1 --network_version=\"rgb2albedo_v01.02_iid\" --iteration=1")
 
 def train_normal():
-    os.system("python \"iid_train_main.py\" --server_config=5 --img_to_load=-1 "
+    os.system("python \"iid_train_main.py\" --server_config=2 --img_to_load=-1 "
               "--plot_enabled=1 --save_per_iter=500 --network_version=\"rgb2normal_v01.01_v05_iid\" --iteration=1")
 
 def train_img2img():
