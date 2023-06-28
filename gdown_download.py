e3d1f8d0de6a630d391fd5afe703f64495835ac5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-28 16:24:10 +0800

    Updated download for titan3

--- gdown_download.py
+++ gdown_download.py
@@ -22,13 +22,13 @@ def main(argv):
     # url = "https://drive.google.com/drive/folders/1mCTQAh_sVFO3UtYXwlYm8_UGTTEwJ_XE?usp=sharing"
     # gdown.download_folder(url, output=output_dir, use_cookies=False)
     #
-    # # v07_iid_base
-    # url = "https://drive.google.com/drive/folders/1mGu-BYkBTTZKx52Es3u8ztgIjA8KiiSU?usp=sharing"
-    # gdown.download_folder(url, output=output_dir, use_cookies=False)
+    # v07_iid_base
+    url = "https://drive.google.com/drive/folders/1mGu-BYkBTTZKx52Es3u8ztgIjA8KiiSU?usp=sharing"
+    gdown.download_folder(url, output=output_dir, use_cookies=False)
 
     # v08_iid_base
-    url = "https://drive.google.com/drive/folders/1mnr806Vw8YqaCKuQGerAboEsz6rO9fgI?usp=sharing"
-    gdown.download_folder(url, output=output_dir, use_cookies=False)
+    # url = "https://drive.google.com/drive/folders/1mnr806Vw8YqaCKuQGerAboEsz6rO9fgI?usp=sharing"
+    # gdown.download_folder(url, output=output_dir, use_cookies=False)
 
     # v89_istd
     # direct_link = "https://drive.google.com/file/d/1mlDSgKT_TZKSqpt6taETHKaOVHpGzRuy/view?usp=sharing"
