03f90090504ff0588a6624ccdbd1a47bc9ac52b5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-29 19:48:05 +0800

    Updated download script.

--- slurm_download.sh
+++ slurm_download.sh
@@ -58,13 +58,13 @@ fi
 #echo "$OUTPUT_DIR/$DATASET_NAME.zip"
 #unzip "$OUTPUT_DIR/$DATASET_NAME.zip" -d "$OUTPUT_DIR"
 #
-#DATASET_NAME="v06_iid_base/v06_iid"
-#zip -F "$OUTPUT_DIR/$DATASET_NAME.zip" --out "$OUTPUT_DIR/$DATASET_NAME+fixed.zip"
-#unzip "$OUTPUT_DIR/$DATASET_NAME+fixed.zip" -d "$OUTPUT_DIR"
-
-DATASET_NAME="v07_iid_base/v07_iid"
+DATASET_NAME="v06_iid_base/v06_iid"
 zip -F "$OUTPUT_DIR/$DATASET_NAME.zip" --out "$OUTPUT_DIR/$DATASET_NAME+fixed.zip"
 unzip "$OUTPUT_DIR/$DATASET_NAME+fixed.zip" -d "$OUTPUT_DIR"
+
+#DATASET_NAME="v07_iid_base/v07_iid"
+#zip -F "$OUTPUT_DIR/$DATASET_NAME.zip" --out "$OUTPUT_DIR/$DATASET_NAME+fixed.zip"
+#unzip "$OUTPUT_DIR/$DATASET_NAME+fixed.zip" -d "$OUTPUT_DIR"
 
 #DATASET_NAME="v08_iid_base/v08_iid"
 #zip -F "$OUTPUT_DIR/$DATASET_NAME.zip" --out "$OUTPUT_DIR/$DATASET_NAME+fixed.zip"
