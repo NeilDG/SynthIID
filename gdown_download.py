9d230827a52d3a5daa47ad85514efe74d8451782
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-07-01 20:41:56 +0800

    Updates on download.

--- gdown_download.py
+++ gdown_download.py
@@ -19,8 +19,8 @@ def main(argv):
         output_dir = "/home/jupyter-neil.delgallego/SynthV3_Raw/"
 
     # v06_iid_base
-    url = "https://drive.google.com/drive/folders/1mCTQAh_sVFO3UtYXwlYm8_UGTTEwJ_XE?usp=sharing"
-    gdown.download_folder(url, output=output_dir, use_cookies=False)
+    # url = "https://drive.google.com/drive/folders/1mCTQAh_sVFO3UtYXwlYm8_UGTTEwJ_XE?usp=sharing"
+    # gdown.download_folder(url, output=output_dir, use_cookies=False)
 
     # v07_iid_base
     url = "https://drive.google.com/drive/folders/1mGu-BYkBTTZKx52Es3u8ztgIjA8KiiSU?usp=sharing"
