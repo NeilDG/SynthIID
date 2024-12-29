b91e0012e0eef03ace98c3b5dd5019de70825561
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-12-29 17:15:12 +0800

    Bug fixes.

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -15,23 +15,13 @@ def train_albedo():
 
 def test_albedo():
     os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
-              "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.02_iid\" --iteration=1")
+              "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.08_iid\" --iteration=1")
 
-    # os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
-    #           "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.03_iid\" --iteration=1")
-    #
-    # os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
-    #           "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.04_iid\" --iteration=1")
-    #
-    # os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
-    #           "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.05_iid\" --iteration=1")
-    #
-    # os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
-    #           "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.06_iid\" --iteration=1")
-    #
     os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
-              "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.07_iid\" --iteration=1")
+              "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.09_iid\" --iteration=1")
 
+    os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 "
+              "--img_vis_enabled=0 --network_version=\"rgb2albedo_v01.10_iid\" --iteration=1")
 def plot_test():
     os.system("python \"plot_test_util.py\" --server_config=5 --img_to_load=-1 --img_vis_enabled=1")
 
@@ -39,11 +29,11 @@ def train_normal():
     # os.system("python \"iid_train_main.py\" --server_config=5 "
     #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2normal_v01.02_iid\" --iteration=1")
 
-    os.system("python \"iid_train_main.py\" --server_config=5 --plot_enabled=0 --save_per_iter=1000 "
-              "--network_version=\"rgb2normal_v01.07_iid\" --iteration=1")
+    # os.system("python \"iid_train_main.py\" --server_config=5 --plot_enabled=0 --save_per_iter=1000 "
+    #           "--network_version=\"rgb2normal_v01.07_iid\" --iteration=1")
 
     os.system("python \"iid_train_main.py\" --server_config=5 --plot_enabled=0 --save_per_iter=1000 "
-              "--network_version=\"rgb2normal_v01.03_iid\" --iteration=1")
+              "--network_version=\"rgb2normal_v01.08_iid\" --iteration=1")
 
     # os.system("python \"iid_train_main.py\" --server_config=5 --plot_enabled=0 --save_per_iter=1000 "
     #           "--network_version=\"rgb2normal_v01.04_iid\" --iteration=1")
@@ -67,18 +57,18 @@ def test_normal():
     #           "--img_vis_enabled=0 --network_version=\"rgb2normal_v01.06_iid\" --iteration=1")
 
     os.system("python \"iid_test_main.py\" --server_config=5 --img_to_load=1000 --test_mode=\"normal\" "
-              "--img_vis_enabled=0 --network_version=\"rgb2normal_v01.07_iid\" --iteration=1")
+              "--img_vis_enabled=1 --network_version=\"rgb2normal_v01.07_iid\" --iteration=1")
 
 def train_img2img():
     os.system("python \"train_img2img_main.py\" --server_config=3 --img_to_load=-1 "
               "--plot_enabled=1 --save_per_iter=50 --network_version=\"synth2istd_v01.00\" --iteration=1")
 
 def main():
-    train_albedo()
+    # train_albedo()
     # test_albedo()
     # plot_test()
 
-    # train_normal()
+    train_normal()
     # test_normal()
     # train_img2img()
     # os.system("shutdown /s /t 1")
